c2ea78734648f8839c404e0a3aaa1afc8710a298
Pranav <pranav7050kumar@gmail.com>
2025-05-15 02:57:11 +0530

    code commit

---
+++ android/app/src/main/kotlin/com/example/firebase_tuto/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.firebase_tuto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "fir-tutorial-dcd76";
+  static String apiKey = "AIzaSyD0lAHXgKGpWylowrQdhzvrsEW0MQeh4rg";
+  static String messagingSenderId = "75648188782";
+  static String projectId = "fir-tutorial-dcd76";
+  final primaryColor = const Color(0xFFee7b64);
+}
